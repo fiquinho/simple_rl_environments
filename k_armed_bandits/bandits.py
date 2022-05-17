777e3f91bb8fd744737dab0a9895dc233e2dabf6
Federico <ficobogado@gmail.com>
2022-05-17 17:02:22 -0300

    Separated configurations. Added code documentation.

--- k_armed_bandits/bandits.py
+++ k_armed_bandits/bandits.py
@@ -1,20 +1,39 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import List, Tuple
-
+from typing_extensions import Protocol
 import numpy as np
+
+
+class ConfigProtocol(Protocol):
+    """Protocol for all bandits configurations"""
+    num_bandits: int
+    max_steps: int
 
 
 @dataclass
 class BanditsConfig:
-    num_bandits: int = 10
-    max_steps: int = 1000
-    reward_value: int = 10
+    """Base configuration for all bandit environments"""
+    num_bandits: int = 10               # Number of possible actions
+    max_steps: int = 1000               # The length of an episode
+
+
+@dataclass
+class FixedBanditsConfig(BanditsConfig):
+    reward_value: int = 10              # The yielded reward from a bandit
+
+
+@dataclass
+class GaussianBanditsConfig(BanditsConfig):
+    global_reward_mean: float = 0.      # The mean reward fro all bandits
+    global_reward_sigma: float = 1.     # The deviations of the mean reward for all bandits
+    reward_sigma: float = 1.            # The deviations of the reward on each action
 
 
 class Bandits(ABC):
+    """Base class for all bandit environments"""
 
-    def __init__(self, config: BanditsConfig):
+    def __init__(self, config: ConfigProtocol):
         self.config = config
         self.step_num = 0
 
@@ -23,6 +42,7 @@ class Bandits(ABC):
         return self.config.num_bandits
 
     def reset(self):
+        # Reset the environment to start a new episode
         self.step_num = 0
 
     @abstractmethod
@@ -30,6 +50,11 @@ class Bandits(ABC):
         """Get reward from bandit #{action}"""
     
     def step(self, action: int) -> Tuple[List[float], float, bool]:
+        """Take a single action in the environment
+
+        :param action: The bandit to use on this time step
+        :return: (environment_sate, reward, is_episode_done)
+        """
         reward = self.get_reward(action)
         self.step_num += 1
         done = self.step_num >= self.config.max_steps
@@ -37,12 +62,18 @@ class Bandits(ABC):
 
 
 class FixedValueBandits(Bandits):
+    """Bandit environment where each bandit has a fixed probability of
+    yielding a reward at any time step. The reward returned is fixed and
+    the same for all bandits."""
 
-    def __init__(self, config: BanditsConfig):
+    config: FixedBanditsConfig
+
+    def __init__(self, config: FixedBanditsConfig):
         super().__init__(config)
         self.probabilities = np.random.uniform(0.001, 1, self.num_bandits)
 
     def get_reward(self, action: int) -> float:
+        """Get reward from bandit #{action}"""
         action_roll = np.random.uniform(0.001, 1)
         if action_roll >= self.probabilities[action]:
             return self.config.reward_value
@@ -51,19 +82,30 @@ class FixedValueBandits(Bandits):
 
 
 class GaussianValueBandits(Bandits):
+    """Bandit environment where each bandit yields a reward each time
+    is selected. Each bandit has a mean reward that its fixed, and
+    generated when the environment is created, from a normal distribution.
+    When a bandit is selected the reward is generated from a normal
+    distribution, with mu equal to the bandits fixed mean reward."""
 
-    def __init__(self, config: BanditsConfig):
+    config: GaussianBanditsConfig
+
+    def __init__(self, config: GaussianBanditsConfig):
         super().__init__(config)
-        self.rewards = np.random.normal(size=self.num_bandits)
+        self.rewards = np.random.normal(loc=self.config.global_reward_mean,
+                                        scale=self.config.global_reward_sigma,
+                                        size=self.num_bandits)
 
     def get_reward(self, action: int) -> float:
-        reward = np.random.normal(self.rewards[action])
+        """Get reward from bandit #{action}"""
+        reward = np.random.normal(loc=self.rewards[action],
+                                  scale=self.config.reward_sigma)
         return reward
 
 
 def main():
     print("Fixed bandits")
-    f_bandits = FixedValueBandits(BanditsConfig(max_steps=20))
+    f_bandits = FixedValueBandits(FixedBanditsConfig(max_steps=20))
     
     done = False
     while not done:
@@ -72,7 +114,7 @@ def main():
             print(reward)
 
     print("Gaussian bandits")
-    g_bandits = GaussianValueBandits(BanditsConfig(max_steps=20))
+    g_bandits = GaussianValueBandits(GaussianBanditsConfig(max_steps=20))
 
     done = False
     while not done:
